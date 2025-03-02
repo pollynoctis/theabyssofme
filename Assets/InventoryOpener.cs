9173ac02e91f55fa39e17df7c63f3228a7ae2d5b
pollynoctis <fedotovapolina0@gmail.com>
2025-03-02 17:05:03 +0200

    Moving scripts to correct folder, inventory opening system in process

---
+++ Assets/InventoryOpener.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryOpener : MonoBehaviour
+{
+    [SerializeField] private GameObject inventoryHolder;
+    /*void Start()
+    {
+        inventoryHolder.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            inventoryHolder.SetActive(!inventoryHolder.activeSelf);
+        }
+    }*/
+}
