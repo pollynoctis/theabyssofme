9a6fb88c4712849b0b7669596ecf5ea78fac0337
pollynoctis <fedotovapolina0@gmail.com>
2025-04-01 21:46:03 +0300

    Inactivity manager

---
+++ Assets/Scripts/InactivityExecutioner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InactivityExecutioner : InactivityManager
+{
+    protected override void InacitvityRelatedAction()
+    {
+        inactivityRelatedObject.SetActive(true);
+    }
+}
+
+
+
---
+++ Assets/Scripts/InactivityManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InactivityManager : MonoBehaviour
+{
+    public GameObject inactivityRelatedObject;
+    public float inactivityTime;
+    
+    private float timer = 0f;
+    private Vector3 lastMousePosition;
+
+    private void Update()
+    {
+        bool mouseMoved = lastMousePosition != Input.mousePosition;
+        lastMousePosition = Input.mousePosition;
+        
+        
+        bool isActive = Input.anyKey || mouseMoved || Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+
+        if (isActive)
+        {
+            ResetTimer();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= inactivityTime)
+            {
+                InacitvityRelatedAction();
+            }
+        }
+    }
+    private void ResetTimer()
+    {
+        timer = 0f;
+        if (inactivityRelatedObject.activeSelf) inactivityRelatedObject.SetActive(false);
+    }
+    protected abstract void InacitvityRelatedAction();
+}
+
