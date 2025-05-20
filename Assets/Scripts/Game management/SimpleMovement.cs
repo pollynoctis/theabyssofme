35bedbc89533720380e65d01819e6b285274d533
pollynoctis <fedotovapolina0@gmail.com>
2025-05-21 00:21:48 +0300

    Milly animations, tests, interactables

--- Assets/Scripts/Game management/SimpleMovement.cs
+++ Assets/Scripts/Game management/SimpleMovement.cs
@@ -11,20 +11,45 @@ public class SimpleMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float accelaration = 2f;
-    
+
+    [SerializeField] private GameObject playerFront, playerSide, playerSideFlipped,playerBack;
+
     private Vector2 movement;
 
     public bool isRunning;
 
     void Update()
     {
-        anim.SetFloat("MovementX", Input.GetAxisRaw("Horizontal"));
+        /*anim.SetFloat("MovementX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MovementY", Input.GetAxisRaw("Vertical"));
-        
-        renderer.flipX = Input.GetAxisRaw("Horizontal") < 0;
-        
+        renderer.flipX = Input.GetAxisRaw("Horizontal") < 0;*/
+        //executionerMovement.LateMove(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement.x < 0)
+        {
+            playerSideFlipped.SetActive(true);
+            playerSide.SetActive(false); playerBack.SetActive(false); playerFront.SetActive(false);
+        }
+        if (movement.x > 0 )
+        {
+            playerSide.SetActive(true);
+            playerSideFlipped.SetActive(false); playerBack.SetActive(false); playerFront.SetActive(false);
+        }
+        if (movement.y < 0)
+        {
+            playerFront.SetActive(true);
+            playerSideFlipped.SetActive(false); playerSide.SetActive(false); playerBack.SetActive(false);
+        } 
+        if (movement.y > 0)
+        {
+            playerBack.SetActive(true);
+            
+            
+            playerSideFlipped.SetActive(false); playerSide.SetActive(false); playerFront.SetActive(false);
+        }
         
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -36,6 +61,6 @@ public class SimpleMovement : MonoBehaviour
             rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
             isRunning = false;
         }
-        //executionerMovement.LateMove(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
     }
 }
