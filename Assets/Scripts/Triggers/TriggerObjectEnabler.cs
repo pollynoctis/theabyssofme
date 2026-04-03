0bcd8cb95308c16a0963e990cd434a85a9135495
polly noctis <fedotovapolina0@gmail.com>
2026-04-03 23:02:50 +0300

    Update 03/04

--- Assets/Scripts/Triggers/TriggerObjectEnabler.cs
+++ Assets/Scripts/Triggers/TriggerObjectEnabler.cs
@@ -8,6 +8,12 @@ public class TriggerObjectEnabler : ParentTriggerObject
     //script for trigger objects
     [SerializeField] private GameObject objectToEnable;
     private bool playedOnce = false;
+
+    private void Start()
+    {
+        objectToEnable.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!playedOnce && other.CompareTag("Player"))
